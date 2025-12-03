c38c71c68707a5532c736b7ed95ef6fcb950384a
xant1ko <semasavasa@gmail.com>
2025-12-03 21:42:20 +0700

    feat: add test component

---
+++ src/copmonents/pages/TestComponent.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="blur-container main-card" :class="{ expanded: OpenedCard }">
+
+        <div @click="togleValue">
+            Открой меня!
+        </div>
+        <div class="description" style="color: aqua;" :class="{ expanded: OpacityDescription }"
+            v-if="enableDescription">
+            Описание
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+
+const OpenedCard = ref(false)
+const enableDescription = ref(false)
+const OpacityDescription = ref(false)
+
+function togleValue() {
+
+    if (OpenedCard.value == false) {
+        // Открытие
+        OpenedCard.value = !OpenedCard.value
+        setTimeout(() => {
+            enableDescription.value = !enableDescription.value
+        }, 500)
+        setTimeout(() => {
+            OpacityDescription.value = !OpacityDescription.value
+        }, 530)
+    } else {
+        OpacityDescription.value = !OpacityDescription.value
+        setTimeout(() => {
+            enableDescription.value = !enableDescription.value
+        }, 300)
+        setTimeout(() => {
+            OpenedCard.value = !OpenedCard.value
+        }, 330)
+        
+    }
+}
+
+</script>
+
+
+
+<style>
+.main-card {
+    width: 400px;
+    height: 50px;
+    transition: 0.5s;
+}
+
+.main-card.expanded {
+    height: 200px;
+}
+
+.description {
+    opacity: 0.1;
+    transition: 0.3s;
+}
+
+.description.expanded {
+    opacity: 1;
+}
+
+div {
+    font-size: 40px !important;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import MainPage from '@/copmonents/pages/MainPage.vue'
 import MenuPage from '@/copmonents/pages/MenuPage.vue'
 import BarMenuPage from '@/copmonents/pages/BarMenuPage.vue'
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import TestComponent from '@/copmonents/pages/TestComponent.vue'
 
 const routes: Array<RouteRecordRaw>=[
   {
@@ -18,7 +19,13 @@ const routes: Array<RouteRecordRaw>=[
     name:'BarMenuPage',
     path:'/barmenu',
     component: BarMenuPage
-  }
+  },
+  {
+    name:'Test',
+    path:'/test',
+    component: TestComponent
+  },
+
 ]
 
 const router = createRouter({
